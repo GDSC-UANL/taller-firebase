bf9517d0d061ce5ba61010a01a45b01ec9a50f95
Fernando Maldonado <f_maldonado6@hotmail.com>
2021-09-10 18:30:54 -0500

    Taller terminado

---
+++ js/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDoynVGuI-lmZG7Ph0Ch1ckQc3IA38_fLs",
+  authDomain: "chat-taller.firebaseapp.com",
+  projectId: "chat-taller",
+  storageBucket: "chat-taller.appspot.com",
+  messagingSenderId: "778459203590",
+  appId: "1:778459203590:web:040cc7bb23fce37ff45b87",
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
